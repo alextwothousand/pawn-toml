8e9d4455aeeae990d4fead8ec39370cc6964beef
alex <38814044+alextwothousand@users.noreply.github.com>
2021-02-09 17:37:51 +0000

    remove maddinat0r sdk submodule

---
+++ source/natives.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 08/02/2021.
+//
+
+#ifndef PAWN_TOML_NAITVES_H
+#define PAWN_TOML_NAITVES_H
+
+#endif //PAWN_TOML_NAITVES_H
---
+++ source/pawn-natives.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 08/02/2021.
+//
+
+#ifndef PAWN_TOML_PAWN_NATIVES_H
+#define PAWN_TOML_PAWN_NATIVES_H
+
+#endif //PAWN_TOML_PAWN_NATIVES_H
